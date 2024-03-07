59c8e16db68589b2d6f1dd83fc51b9e3ef02ff63
SheilaTran-UCF <minhngocny@hotmail.com>
2024-03-07 20:59:12 +0700

    push

--- src/components/SchoolComponents/TabsComponent.jsx
+++ src/components/SchoolComponents/TabsComponent.jsx
@@ -11,6 +11,7 @@ import icon3 from '../../assets/icon3.svg'
 import icon4 from '../../assets/icon4.svg'
 import icon5 from '../../assets/icon5.svg'
 
+
 function TabsComponent(props) {
 	console.log(ImagesPic.ca1);
 
@@ -127,7 +128,7 @@ export default function BasicTabs() {
             <div className='flex justify-between items-center '>
             <div className='mx-[4px]'>
               <img 
-              width='80%'
+              className='bg-auto'
               src={icon4} alt="" />
             </div>
             <div className='mx-[10px] '>
@@ -142,9 +143,9 @@ export default function BasicTabs() {
           className='mx-[6px] rounded-full'
           label={
             <div className='flex justify-between items-center '>
-            <div className='mx-[4px]'>
+            <div className='mx-[4px] w-[80%]'>
               <img
-              width='80%'
+              className='bg-auto'
               src={icon5} alt="" />
             </div>
             <div className='mx-[10px] '>
@@ -164,7 +165,52 @@ export default function BasicTabs() {
 
 			<TabsComponent value={value} index={0} 
       className='my-[30px] '>
-				<div className='grid grid-cols-12 gap-3 my-[10px]'>
+        <div className='flex justify-center flex-wrap'>
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          <div className='w-[48%] md:w-[23%] px-[1%] py-2'>
+          <img 
+            className='py-[5px] w-[100%] ' src={ImagesPic.ca1} alt='' />
+          </div>
+          
+          
+          
+          
+        </div>
+				{/* <div className='grid grid-cols-12 gap-3 my-[10px]'>
 					<div className='col-span-3 '>
 						<img 
             className='py-[5px]' src={ImagesPic.ca1} alt='' />
@@ -192,20 +238,136 @@ export default function BasicTabs() {
           <div className='col-span-3'>
 						<img width='80%' src={ImagesPic.ca8} alt='' />
 					</div>
+				</div> */}
+
+
+			</TabsComponent>
+
+      <TabsComponent value={value} index={1} 
+      className='my-[30px] '>
+				<div className='grid grid-cols-12 gap-3 my-[10px]'>
+					<div className='col-span-3 '>
+						<img 
+            className='py-[5px]' src={ImagesPic.u1} alt='' />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u2} />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u3} />
+					</div>
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u4} />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u5} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u6} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.u7} alt='' />
+					</div>
+          
 				</div>
 			</TabsComponent>
-			<TabsComponent value={value} index={1}>
-				<div>United</div>
+			
+
+
+      <TabsComponent value={value} index={2} 
+      className='my-[30px] '>
+				<div className='grid grid-cols-12 gap-3 my-[10px]'>
+					<div className='col-span-3 '>
+						<img 
+            className='py-[5px]' src={ImagesPic.uk1} alt='' />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk2} />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk4} />
+					</div>
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk5} />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk6} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk7} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk8} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.uk9} alt='' />
+					</div>
+				</div>
 			</TabsComponent>
-			<TabsComponent value={value} index={2}>
-				Item Three
+
+     
+
+      <TabsComponent value={value} index={3} 
+      className='my-[30px] '>
+				<div className='grid grid-cols-12 gap-3 my-[10px]'>
+					<div className='col-span-3 '>
+						<img 
+            className='py-[5px]' src={ImagesPic.a1} alt='' />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.a2} />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.a3} />
+					</div>
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.a4} />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.a5} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.a6} alt='' />
+					</div>
+         
+				</div>
 			</TabsComponent>
-			<TabsComponent value={value} index={3}>
-				Item Four
+			
+
+      <TabsComponent value={value} index={4} 
+      className='my-[30px] '>
+				<div className='grid grid-cols-12 gap-3 my-[10px]'>
+					<div className='col-span-3 '>
+						<img 
+            className='py-[5px]' src={ImagesPic.i1} alt='' />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.i2} />
+					</div>
+
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.i3} />
+					</div>
+					<div className='col-span-3'>
+						<img width='80%' src={ImagesPic.i4} />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.i5} alt='' />
+					</div>
+          <div className='col-span-3'>
+						<img width='80%' src={ImagesPic.i6} alt='' />
+					</div>
+         
+				</div>
 			</TabsComponent>
-			<TabsComponent value={value} index={4}>
-				Item Five
-			</TabsComponent>
+		
 		</Box>
 	);
 }
--- src/config/ImagesExport.js
+++ src/config/ImagesExport.js
@@ -15,6 +15,28 @@ import U5 from '../assets/U5.webp'
 import U6 from '../assets/U6.webp'
 import U7 from '../assets/U7.webp'
 
+import UK1 from '../assets/UK1.webp'
+import UK2 from '../assets/UK2.webp'
+import UK4 from '../assets/UK4.webp'
+import UK5 from '../assets/UK5.webp'
+import UK6 from '../assets/UK6.webp'
+import UK7 from '../assets/UK7.webp'
+import UK8 from '../assets/UK8.webp'
+import UK9 from '../assets/UK9.webp'
+
+
+import A1 from '../assets/A1.webp'
+import A2 from '../assets/A2.webp'
+import A3 from '../assets/A3.webp'
+import A4 from '../assets/A4.webp'
+import A5 from '../assets/A5.webp'
+import A6 from '../assets/A6.webp'
+
+import I1 from '../assets/I1.webp'
+import I2 from '../assets/I2.webp'
+import I3 from '../assets/I3.webp'
+import I4 from '../assets/I4.webp'
+import I5 from '../assets/I5.webp'
 
 
 
@@ -29,13 +51,38 @@ const ImagesPic = {
     ca7 : Ca7,
     ca8 : Ca8,
 
-    u1 :U1,
+    u1 : U1,
     u2 : U2,
     u3 : U3,
     u4 : U4,
     u5 : U5,
     u6 : U6,
     u7 : U7,
+
+    uk1 : UK1,
+    uk2 : UK2,
+    uk5 : UK5,
+    uk4 : UK4,
+    uk6 : UK6,
+    uk7 : UK7,
+    uk8 : UK8,
+    uk9 : UK9,
+
+
+    a1 : A1,
+    a2 : A2,
+    a3 : A3,
+    a4 : A4,
+    a5 : A5,
+    a6 : A6,
+
+
+    i1 : I1,
+    i2 : I2,
+    i3 : I3,
+    i4 : I4,
+    i5 : I5
+
 
 }
 
